0553a71ed65c7d80b0b418e568f751540191e439
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-05-30 22:12:26 +0300

    гз

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,12 @@
-
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-
-
-
-const iframe = document.querySelector('iframe')
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
-player.on('timeupdate', throttle(data=> {
-    localStorage.setItem('videoplayer-current-time',data.seconds)
-}, 1000));
-
+player.on('timeupdate', throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
 const currentTime = localStorage.getItem('videoplayer-current-time');
- if (currentTime!==null) {
-    player.setCurrentTime(currentTime)
- }
-
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime);
+};
