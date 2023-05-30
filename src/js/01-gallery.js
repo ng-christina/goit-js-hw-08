f27f305e96894fea00e7de9ba52667bb60d3f340
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-05-30 17:27:49 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 // Описаний в документації
 import SimpleLightbox from "simplelightbox";
