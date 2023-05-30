ec9ea2ff76af325c94fc52c3b57b5975d725ec2d
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-05-30 17:23:48 +0300

    up1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";  
+//////
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const fotoCard = galleryFoto(galleryItems) 
+ gallery.insertAdjacentHTML('beforeend', fotoCard);
+
+ function galleryFoto(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description }) =>{
+        return`
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+               <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+        })
+    .join('');
+
+ }
+ const leigthBox = new SimpleLightbox(".gallery__link", { 
+    navText : ['<','>'],
+    captionsData: 'alt',
+    captionDelay : 250
+})
+
+  gallery.addEventListener("click", galleryClick);
+ function galleryClick(evt) {
+  evt.preventDefault();
+ }
